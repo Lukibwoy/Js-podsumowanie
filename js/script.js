cf2f854788eebdd7a5a191ce6d19463446ce9b69
Lukibwoy <lukaszwiczynski@gmail.com>
2023-05-29 21:10:31 +0200

    updated tasks

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ fetch(commentsAPI)
 			const deleteButton = document.createElement('button')
 			const editButton = document.createElement('button')
 			const checkbox = document.createElement('input')
+			const textArea = document.createElement('textarea')
+			const saveBtn = document.createElement('button')
 			checkbox.type = 'checkbox'
 
 			name.textContent = comment.name
@@ -22,6 +24,8 @@ fetch(commentsAPI)
 			body.textContent = comment.body
 			editButton.textContent = 'Edit'
 			deleteButton.textContent = 'Delete'
+			saveBtn.textContent = 'Save'
+			saveBtn.style.display = 'none'
 
 			action.appendChild(deleteButton)
 			newRow.appendChild(name)
@@ -30,6 +34,16 @@ fetch(commentsAPI)
 			newRow.appendChild(action)
 			tableBody.appendChild(newRow)
 			action.appendChild(checkbox)
+			action.appendChild(editButton)
+			action.appendChild(saveBtn)
+
+			// flow do edycji
+			// 1. klikamy edit
+			// 2. pojawia się textarea w miejscu body
+			// 3. wpisujemy z klawiatury zmiany do textarea
+			// 4. klikamy save
+			// 5. tekst z textarea pojawia się w body i znika textarea
+			// bonus: jeśliw widoczny jest save to nie widać edit i na odwrót
 
 			//usuwanie pojedynczego komentarza
 			const deleteRow = row => {
@@ -38,6 +52,25 @@ fetch(commentsAPI)
 
 			deleteButton.addEventListener('click', () => {
 				deleteRow(newRow)
+			})
+
+			editButton.addEventListener('click', () => {
+				const textArea = document.createElement('textarea')
+				textArea.style.overflow = 'hidden'
+				textArea.style.width = '100%'
+				const saveButton = document.createElement('button')
+
+				textArea.value = body.textContent
+				newRow.replaceChild(textArea, body)
+				action.replaceChild(saveButton, editButton)
+
+				saveButton.textContent = 'Save'
+
+				saveButton.addEventListener('click', () => {
+					body.textContent = textArea.value
+					newRow.replaceChild(body, textArea)
+					action.replaceChild(editButton, saveButton)
+				})
 			})
 
 			//usuwanie wybranych
@@ -81,43 +114,89 @@ input.addEventListener('keyup', searchColor)
 
 // /zad.3//////////////////////////////////////////////////////////////////////////
 
-const apiUrl = 'https://api.unsplash.com/photos/random?count=12&client_id=aQZHGKWvzhRJdQvULsszq4Vwf8LxH5tJbODWl9A9EUo'
+// const apiUrl = 'https://api.unsplash.com/photos/random?count=9&client_id=aQZHGKWvzhRJdQvULsszq4Vwf8LxH5tJbODWl9A9EUo'
 
-async function fetchPhotos() {
-	try {
-		const response = await fetch(apiUrl)
-		const data = await response.json()
-		return data
-	} catch (error) {
-		console.log('Błąd pobierania zdjęć:', error)
-	}
-}
+// async function fetchPhotos() {
+// 	try {
+// 		const response = await fetch(apiUrl)
+// 		const data = await response.json()
+// 		return data
+// 	} catch (error) {
+// 		console.log('Błąd pobierania zdjęć:', error)
+// 	}
+// }
 
-function createGallery(data) {
-	const galleryElement = document.querySelector('.gallery')
+// function createGallery(data) {
+// 	const galleryElement = document.querySelector('.gallery')
 
-	const galleryImages = data.map(photo => {
-		const photoContainer = document.createElement('div')
-		photoContainer.classList.add('photo-container')
+// 	const galleryImages = data.map(photo => {
+// 		const photoContainer = document.createElement('div')
+// 		photoContainer.classList.add('photo-container')
 
-		const imgElement = document.createElement('img')
-		imgElement.src = photo.urls.regular
+// 		const imgElement = document.createElement('img')
+// 		imgElement.src = photo.urls.regular
 
-		const authorNameElement = document.createElement('span')
-		authorNameElement.classList.add('author-name')
-		authorNameElement.textContent = photo.user.name
+// 		const authorNameElement = document.createElement('span')
+// 		authorNameElement.classList.add('author-name')
+// 		authorNameElement.textContent = photo.user.name
 
-		photoContainer.appendChild(imgElement)
-		photoContainer.appendChild(authorNameElement)
+// 		photoContainer.appendChild(imgElement)
+// 		photoContainer.appendChild(authorNameElement)
 
-		return photoContainer
+// 		return photoContainer
+// 	})
+
+// 	galleryElement.append(...galleryImages)
+// }
+
+// fetchPhotos().then(data => {
+// 	createGallery(data)
+// })
+
+//Zad.4/////////////////////////////////////////////////////////////////////////////
+
+const formJson = [
+	{ fieldName: 'Imie', fieldType: 'text', validationRegex: '[a-zA-Z]+' },
+	{ fieldName: 'Nazwisko', fieldType: 'text', validationRegex: '[a-zA-Z]+' },
+	{ fieldName: 'Hasło', fieldType: 'password', validationRegex: '^.{8,}$' },
+	{ fieldName: 'Email', fieldType: 'email', validationRegex: '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}' },
+]
+
+function generateForm(jsonData) {
+	const form = document.createElement('form')
+
+	jsonData.forEach(field => {
+		const label = document.createElement('label')
+		label.setAttribute('for', field.fieldName)
+		label.textContent = field.fieldName + ':'
+
+		const input = document.createElement('input')
+		input.setAttribute('type', field.fieldType)
+		input.setAttribute('id', field.fieldName)
+		input.setAttribute('name', field.fieldName)
+		input.setAttribute('pattern', field.validationRegex)
+		input.required = true
+
+		const br = document.createElement('br')
+
+		form.appendChild(label)
+		form.appendChild(input)
+		form.appendChild(br)
 	})
 
-	galleryElement.append(...galleryImages)
+	const submitButton = document.createElement('input')
+	submitButton.setAttribute('type', 'submit')
+	submitButton.setAttribute('value', 'Wyślij')
+
+	form.appendChild(submitButton)
+
+	return form
 }
 
-fetchPhotos().then(data => {
-	createGallery(data)
+document.addEventListener('DOMContentLoaded', function () {
+	const formContainer = document.getElementById('form-container')
+	const dynamicForm = generateForm(formJson)
+	formContainer.appendChild(dynamicForm)
 })
 
 // /Zad.5///////////////////////////////////////////////////////////////////////////
@@ -132,14 +211,18 @@ function zoomAndMoveImg() {
 		legoImg.addEventListener('mousemove', move)
 	} else {
 		legoImg.removeEventListener('mousemove', move)
+		legoImg.style.backgroundPositionX = 0
+		legoImg.style.backgroundPositionY = 0
 	}
 }
 
 function move() {
 	legoImg.addEventListener('mousemove', e => {
 		if (isZoomed) {
-			legoImg.style.backgroundPositionX = -e.offsetX + 'px'
-			legoImg.style.backgroundPositionY = -e.offsetY + 'px'
+			const desiredPositionX = -e.offsetX + 390
+			const desiredPositionY = -e.offsetY + 370
+			legoImg.style.backgroundPositionX = `${desiredPositionX}px`
+			legoImg.style.backgroundPositionY = `${desiredPositionY}px`
 		}
 	})
 }
@@ -150,31 +233,31 @@ legoImg.addEventListener('click', zoomAndMoveImg)
 
 const meetings = [
 	{
-		date: '2022-05-09',
+		date: '27-5-2023',
 		time: '10:00',
 		name: 'Spotkanie z XYZ',
 		client: 'Jan Kowalski',
 	},
 	{
-		date: '2022-05-10',
+		date: '30-5-2023',
 		time: '12:00',
 		name: 'Spotkanie z ABC',
 		client: 'Anna Nowak',
 	},
 	{
-		date: '2022-05-11',
+		date: '19-6-2023',
 		time: '14:00',
 		name: 'Spotkanie z KLM',
 		client: 'Adam Nowakowski',
 	},
 	{
-		date: '2022-05-12',
+		date: '5-6-2023',
 		time: '16:30',
 		name: 'Spotkanie z DEF',
 		client: 'Ewa Nowakowska',
 	},
 	{
-		date: '2022-05-13',
+		date: '11-6-2023',
 		time: '9:00',
 		name: 'Spotkanie z GHI',
 		client: 'Piotr Kowalczyk',
@@ -203,16 +286,34 @@ function renderCalendar() {
 		row.appendChild(dayCell)
 
 		const timeCell = document.createElement('td')
-		timeCell.textContent = ''
+
 		row.appendChild(timeCell)
 
 		const nameCell = document.createElement('td')
-		nameCell.textContent = ''
+		// nameCell.textContent = ''
 		row.appendChild(nameCell)
 
 		const clientCell = document.createElement('td')
-		clientCell.textContent = ''
+		// clientCell.textContent = ''
 		row.appendChild(clientCell)
+
+		// jeżeli w zmiennei meetings jest wpis o takiej samej dacie jak w zmiennej date to textContent ma być ustawiony na tekst z tego wpisu
+		// w przeciwnym wypadku tak jak jest teraz
+		let meetingTime = ''
+		let meetingName = ''
+		let meetingClient = ''
+		for (let j = 0; j < meetings.length; j++) {
+			const meeting = meetings[j]
+			if (meeting.date === formattedDate) {
+				meetingTime = meeting.time
+				meetingName = meeting.name
+				meetingClient = meeting.client
+			}
+		}
+
+		timeCell.textContent = meetingTime
+		nameCell.textContent = meetingName
+		clientCell.textContent = meetingClient
 
 		calendarBody.appendChild(row)
 	}
@@ -230,7 +331,7 @@ function nextWeek() {
 
 function formatDate(date) {
 	const day = String(date.getDate())
-	const month = String(date.getMonth())
+	const month = String(date.getMonth() + 1)
 	const year = date.getFullYear()
 	return `${day}-${month}-${year}`
 }
@@ -344,4 +445,4 @@ function joinWords(word, scale, words) {
 	}
 }
 
-console.log(convertToWords(1112))
+console.log(convertToWords(11212))
